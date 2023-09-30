f7585fe9b7520887878f09e218b750d31581596d
sdfghjka <kxnenao100@yahoo.com.tw>
2023-09-30 15:36:06 +0800

    feat: add search funtion to listing

--- app.js
+++ app.js
@@ -17,14 +17,21 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
-app.get('/restaurants', (req ,res) =>{
-  res.render('index',{restaurants : restaurants})
+app.get('/restaurants', (req, res) => {
+  const keyword = req.query.keyword?.trim()
+  const matchesRestaurant = keyword ? restaurants.filter((restaurant) => Object.values(restaurant).some((property) => {
+    if(typeof property === "string"){
+      return property.toLowerCase().includes(keyword.toLowerCase())
+    }
+    return false
+  })):restaurants
+  res.render('index', { restaurants: matchesRestaurant , keyword})
 
 })
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   const restaurant = restaurants.find((restaurant) => restaurant.id.toString() === id)
-  res.render('detail', { restaurant : restaurant} )
+  res.render('detail', { restaurant: restaurant })
 })
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
