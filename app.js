b320f13a33835bd8ba8d922b27342ad0fc54f8e3
sdfghjka <kxnenao100@yahoo.com.tw>
2024-10-22 20:50:03 +0900

    install & initialize sequelize

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 const port = 3000
 const { engine } = require('express-handlebars')
-const restaurants = require('./public/jsons/restaurant.json').results
-
+// const restaurants = require('./public/jsons/restaurant.json').results
+// 
+const db = require('./models');
+const restaurant = db.Restaurant;
 
 
 
@@ -17,17 +19,25 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
-app.get('/restaurants', (req, res) => {
-  const keyword = req.query.keyword?.trim()
-  const matchesRestaurant = keyword ? restaurants.filter((restaurant) => Object.values(restaurant).some((property) => {
-    if(typeof property === "string"){
-      return property.toLowerCase().includes(keyword.toLowerCase())
-    }
-    return false
-  })):restaurants
-  res.render('index', { restaurants: matchesRestaurant , keyword})
-
-})
+app.get('/restaurants', async (req, res) => {
+  try {
+      const restaurants = await restaurant.findAll(); // 使用 await 獲取資料
+      const rest = restaurants.map(restaurant => restaurant.toJSON());
+      const keyword = req.query.keyword?.trim();
+      const matchesRestaurant = keyword 
+          ? rest.filter((restaurant) => 
+              Object.values(restaurant).some((property) => 
+                  typeof property === "string" && 
+                  property.toLowerCase().includes(keyword.toLowerCase())
+              )
+          ) 
+          : rest;
+      res.render('index', { restaurants: matchesRestaurant, keyword });
+  } catch (error) {
+      console.error(error); // 錯誤處理
+      res.status(500).send('Internal Server Error');
+  }
+});
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   const restaurant = restaurants.find((restaurant) => restaurant.id.toString() === id)
